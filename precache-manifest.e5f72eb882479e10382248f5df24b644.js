53ebeccb6d0d30b233053f0759e0d83c95066978
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-05-25 22:59:52 +0700

    Updates

---
+++ precache-manifest.e5f72eb882479e10382248f5df24b644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad47b1c5aaa4b794ab4146f201ea883",
+    "url": "/Suchanya-Resume/index.html"
+  },
+  {
+    "revision": "8393e79c86858a283f51",
+    "url": "/Suchanya-Resume/static/css/main.64bd4241.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Suchanya-Resume/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8393e79c86858a283f51",
+    "url": "/Suchanya-Resume/static/js/main.2a5cfb1e.chunk.js"
+  },
+  {
+    "revision": "8de22c9fee5e055e3e80",
+    "url": "/Suchanya-Resume/static/js/runtime~main.7e8d66cb.js"
+  },
+  {
+    "revision": "908602cacf86466cda8a3806e18adf59",
+    "url": "/Suchanya-Resume/static/media/77386.908602ca.jpg"
+  }
+]);
